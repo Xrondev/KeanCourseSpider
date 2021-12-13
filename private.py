cb1a8244e7511688a41fb86721a3efe065e4247a
Xrondev <61485096+Xrondev@users.noreply.github.com>
2021-12-13 16:19:59 +0800

    Add files via upload

---
+++ private.py
@@ -0,0 +1,10 @@
+username = 'EXAMPLE'
+password = 'PASSWORD'
+db = {
+    'host': 'ip.ip.ip.ip',
+    'port': 3306,
+    'db': 'db_example',
+    'table': 'table_example',
+    'usr': 'db_usr',
+    'pwd': 'db_pwd',
+}
